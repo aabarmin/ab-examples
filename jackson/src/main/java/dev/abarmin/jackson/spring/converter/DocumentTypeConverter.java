ca1bc1a4c21bb25aff9bbc700c51e3f279ae8ed9
Aleksandr Barmin <abarmin@outlook.com>
2024-05-04 19:01:04 +0100

    Added example for Spring and Jackson

--- jackson/src/main/java/dev/abarmin/jackson/spring/converter/DocumentTypeConverter.java
+++ jackson/src/main/java/dev/abarmin/jackson/spring/converter/DocumentTypeConverter.java
@@ -1,8 +1,12 @@
 package dev.abarmin.jackson.spring.converter;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import dev.abarmin.jackson.spring.model.DocumentType;
 import org.springframework.boot.jackson.JsonComponent;
 
@@ -12,9 +16,18 @@ import java.io.IOException;
 public class DocumentTypeConverter {
     public static class Deserializer extends JsonDeserializer<DocumentType> {
         @Override
-        public DocumentType deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        public DocumentType deserialize(JsonParser p,
+                                        DeserializationContext ctxt) throws IOException, JacksonException {
+
             final String stringValue = ctxt.readValue(p, String.class);
             return DocumentType.byValue(stringValue);
+        }
+    }
+
+    public static class Serializer extends JsonSerializer<DocumentType> {
+        @Override
+        public void serialize(DocumentType value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.getValue());
         }
     }
 }
