ea27c33de4f64af0a289e41ec1608b9cdeab652d
Aleksandr Barmin <abarmin@outlook.com>
2024-08-01 12:47:24 +0100

    Added docker compose support for PostgreSQL

--- spring-test/src/main/java/dev/abarmin/spring/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.abarmin.spring.repository;
-
-import dev.abarmin.spring.entity.AuthorityEntity;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface AuthorityRepository extends ListCrudRepository<AuthorityEntity, String> {
-}
