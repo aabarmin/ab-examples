93d5b250ccb307d373a761c810bac01f57b29775
Aleksandr Barmin <abarmin@outlook.com>
2024-08-11 19:07:49 +0100

    OAuth2 example (#54)

--- spring-test/src/main/java/dev/abarmin/spring/config/SecurityConfiguration.java
+++ spring-test/src/main/java/dev/abarmin/spring/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -66,6 +65,7 @@ public class SecurityConfiguration {
 
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 
